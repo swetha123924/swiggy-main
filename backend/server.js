11012432843ded0c92d0a7d41ef6497c13ea6ca2
maheswari7675 <maheswarireddi053@gmail.com>
2025-06-30 10:12:13 +0530

    swiggy App

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://kaleidoscopic-mandazi-d08669.netlify.app/",
+    credentials: true
+  }
+));
 app.use(express.json());
 
 app.use('/auth', authRoutes);
